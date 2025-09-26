68c563fa83d32b156447d3c8295da26be2e19338
lokesh shriwas <lokeshshriwas2802@gmail.com>
2025-09-27 01:43:10 +0530

    04: background jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
+"use client"
+import { Button } from "@/components/ui/button"
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
 const page = () => {
+  const trpc = useTRPC();
+  const invoke = useMutation(trpc.invoke.mutationOptions({}))
   return (
-    <div>page</div>
+    <div>
+      <Button onClick={() => invoke.mutate({text : "John"})}>invoke backgriund job</Button>
+    </div>  
   )
 }
 
