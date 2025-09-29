adfd8b8091299fe6ac917e634b100f55cd225bfe
lokesh shriwas <lokeshshriwas2802@gmail.com>
2025-09-30 01:13:41 +0530

    added api agent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,15 @@
 import { Button } from "@/components/ui/button"
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
 const page = () => {
+  const [value, setValue] = useState("")
   const trpc = useTRPC();
   const invoke = useMutation(trpc.invoke.mutationOptions({}))
   return (
     <div>
-      <Button onClick={() => invoke.mutate({text : "John"})}>invoke backgriund job</Button>
+      <input type="text" value={value} onChange={(e) => setValue(e.target.value)} />
+      <Button onClick={() => invoke.mutate({value : value})}>invoke background job</Button>
     </div>  
   )
 }
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,24 @@
 import { inngest } from "./client";
+import { createAgent, grok } from "@inngest/agent-kit";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
-  { event: "test/hello.world" },
+  { event: "test/hello-world" },
   async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "1s");
-    return { message: `Hello ${event.data.email}!` };
-  },
+    const summarize = createAgent({
+      name: "summarize",
+      system: "You are an expert at summarizer. You summarize in 2 lines.",
+      model: grok({
+        model: "grok-4-latest",
+        apiKey : process.env.XAI_API_KEY
+      }),
+    });
+
+    const output = await summarize.run(
+      `Summarize the following text ; ${event.data.value}`
+    )
+    console.log(output)
+    return { success : "ok" };
+  }
 );
+
